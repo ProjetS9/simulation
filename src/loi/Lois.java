453ef5e4d267d08a767055ee244f9e6d277cfaad
LIYi <ly595788168@hotmail.com>
2018-01-08 04:43:22 +0100

    add loi

--- src/loi/Lois.java
+++ src/loi/Lois.java
@@ -1,15 +1,29 @@
 package loi;
 
+import java.util.Random;
+
 public class Lois {
+	Random ran = new Random();
 	
-	public double exponentielle(double d){
+	public double exponentielle(double lambda){
 		double minutes = 0;
+		if(lambda >= 0){
+			double p = ran.nextFloat();
+			minutes = - Math.log(1-p) / (double) lambda;
+		}
+		else
+			minutes = -1;
 		return minutes;
 	}
 	
 	
-	public double uniforme(double i, double j){
+	public double uniforme(double debut, double fin){
 		double minutes = 0;
+		if(debut > fin)
+			return minutes = -1;
+		else{
+			minutes = debut + ran.nextFloat() * (fin - debut);
+		}
 		return minutes;
 	}
 }
